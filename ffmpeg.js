66b6b845243715c5399716546844b0f3460072fe
vaaiibhav-pushstart <vaibhav.vaidya@pushstart.in>
2023-03-09 15:03:03 +0530

    crud-Working-Poc

---
+++ ffmpeg.js
@@ -0,0 +1,20 @@
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+console.log(`${__dirname}\video.mp4 :`, `${__dirname}\video.mp4`);
+ffmpeg(`${__dirname}\video.mp4`, { timeout: 432000 })
+  .addOptions([
+    "-profile:v baseline",
+    "-level 3.0",
+    "-start_number 0",
+    "-hls_time 10",
+    "-hls_list_size 0",
+    "-f hls",
+  ])
+  .output("videos/output.m3u8")
+  .on("end", () => {
+    console.log("end");
+  })
+  .run();
